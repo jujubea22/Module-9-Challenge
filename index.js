e934bd5027ad76e47e46433da3f0c51cdbdf8f76
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-01-06 13:32:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,71 +6,50 @@ const generateMD = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions =
 [
-    {
-        type: 'input',
+    { type: 'input',
         name: 'title',
-        message: 'Enter the Title of Project?'
-    },
+        message: 'Enter the Title of Project?'},
 
-    {
-        type: 'input',
+    { type: 'input',
         name: 'description',
-        message: 'Describe your Projects:'
-    },
+        message: 'Describe your Projects:'},
 
-    {
-        type: 'input',
+    { type: 'input',
         name: 'instructions',
-        message: 'Please Enter Instructions:'
-    },
+        message: 'Please Enter Instructions:' },
 
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter Usage Information:'
-    },
-
-    {
-        type: 'input',
+    {type: 'input',
         name: 'contribution',
-        message: 'Please Any Enter Contribution Guidelines:'
-    },
+        message: 'Please Any Enter Contribution Guidelines:' },
 
-    {
-        type: 'input',
+    {type: 'input',
+        name: 'usage',
+        message: 'Enter Usage Information:' },
+    
+    { type: 'input',
         name: 'test',
-        message: 'Enter Test Instructions:'
-    },
+        message: 'Enter Test Instructions:' },
 
-    {
-        type: 'input',
-        name: 'gituser',
-        message: 'Enter Your GitHub UserName:'
-    },
-
-    {
-        type: 'input',
+     {type: 'input',
         name: 'email',
-        message: 'Enter Your Email Address:'
-    },
+        message: 'Enter Your Email Address:'},
+    
+    {type: 'input',
+        name: 'gituser',
+        message: 'Enter Your GitHub UserName:'},
 
-    {
-        type: 'list',
+    {type: 'list',
         name: 'license',
         message: 'Please select any licenses for your Application:',
         choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License',
         'BSD 3-Clause "New" or "Revised" License', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0',
-        'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
-    }
+        'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'}
   ]
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     var dataStr = generateMD(data);
     fs.writeFile(fileName, dataStr, (err) => 
-    err ? console.log(err) : console.log('Thank you, the info you entered has been added to README file')
-    );
-
-
+    err ? console.log(err) : console.log('Thank you, the info you entered has been added to README file'));
 }
 // TODO: Create a function to initialize app
 function init() {
